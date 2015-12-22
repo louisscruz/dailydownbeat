92bf71752cb6ec4df85331ea4407e51e091fb531
louisscruz <louisstephancruz@me.com>
2015-12-22 00:49:39 -0500

    Begin initial form validation

--- src/app/account/login/login.ts
+++ src/app/account/login/login.ts
@@ -1,16 +1,24 @@
 import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from 'angular2/common';
 import {ButtonRadio} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'login',
   template: require('./login.html'),
-  directives: [ ButtonRadio ]
+  directives: [ ButtonRadio, FORM_DIRECTIVES ]
 })
 
 export class Login {
-  private radioModel:string = 'Login';
+  private radioModel: string = 'Login';
+  loginForm: ControlGroup;
 
-  submit() {
-    alert('woohoo');
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      'username': ['', Validators.required]
+    })
+  }
+
+  submit(value: string): void{
+    console.log(value);
   };
 }
