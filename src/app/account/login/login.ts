16df240aa07b6a8aec47dbdf46c87199d87a84b9
louisscruz <louisstephancruz@me.com>
2015-12-23 01:26:15 -0500

    Add email validation to login

--- src/app/account/login/login.ts
+++ src/app/account/login/login.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from 'angular2/common';
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Control, Validators} from 'angular2/common';
 import {ButtonRadio} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
@@ -7,14 +7,20 @@ import {ButtonRadio} from 'ng2-bootstrap/ng2-bootstrap';
   template: require('./login.html'),
   directives: [ ButtonRadio, FORM_DIRECTIVES ]
 })
-
 export class Login {
   private radioModel: string = 'Login';
   loginForm: ControlGroup;
 
   constructor(fb: FormBuilder) {
+    function emailValidator(control: Control): { [s: string]: boolean} {
+      if (!control.value.match(/.+@.+\..+/i)) {
+        return {invalidEmail: true}
+      }
+    }
     this.loginForm = fb.group({
-      'username': ['', Validators.required]
+      'username': ['', Validators.required],
+      'email': ['', Validators.compose([
+        Validators.required, emailValidator])]
     })
   }
 
