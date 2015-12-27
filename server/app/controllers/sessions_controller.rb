7d110d8a6c2b391f7cc57e16d0912399003f48cf
louisscruz <louisstephancruz@me.com>
2015-12-27 18:46:36 -0500

    Clean up session create

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -3,17 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user_password = params[:session][:password]
-    user_email = params[:session][:email]
-    user = user_email.present? && User.find_by(email: user_email)
+    user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
       log_in user
       user.generate_authentication_token!
-      p user.auth_token
-
       user.update_attribute(:auth_token, user.auth_token)
-      p user
       render json: user, status: :created
     else
       render json: user.errors, status: :unprocessable_entity
