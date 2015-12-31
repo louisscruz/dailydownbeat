221c0c8af16b95ef7b5d68d9ad5b5690ffe459b6
louisscruz <louisstephancruz@me.com>
2015-12-30 20:47:49 -0500

    Add basic fs menu

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,13 +1,14 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig, Router} from 'angular2/router';
 import {Http} from 'angular2/http';
+import {Collapse} from 'ng2-bootstrap/ng2-bootstrap';
 
 import {AuthService} from '../services/auth/authService';
 import {AlertService} from '../services/alerts/alertsService';
 
 @Component({
   selector: 'navbar',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, Collapse],
   pipes: [],
   styles: [ require('./navbar.scss') ],
   template: require('./navbar.html'),
@@ -15,8 +16,18 @@ import {AlertService} from '../services/alerts/alertsService';
 })
 export class Navbar {
   isLoggedIn = 'yes';
+  public isCollapsed: boolean = true;
+  public toggleCollapse: any;
+  public collapse: any;
   constructor(private authService: AuthService) {
     this.authService = authService;
+    this.isCollapsed;
+    this.toggleCollapse = function() {
+      this.isCollapsed = !this.isCollapsed;
+    }
+    this.collapse = function() {
+      this.isCollapsed = true;
+    }
     authService.isLoggedIn();
   }
 
