ba4454359e17b8552600175412dbae643603843f
louisscruz <louisstephancruz@me.com>
2016-01-30 01:16:34 -0500

    Add comment ref to postdetail

--- server/app/serializers/comment_serializer.rb
+++ server/app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :body, :commentable_type, :commentable_id
   has_one :user do
-    object.user.username
+    [username: object.user.username, user_id: object.user.id]
   end
 end
