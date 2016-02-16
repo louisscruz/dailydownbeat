6a7912c9a3a305aa3e5efbcd4d0131844dbe3175
louisscruz <louisstephancruz@me.com>
2016-02-15 23:04:41 -0500

    Remove vote_count from serializer

--- server/app/serializers/post_serializer.rb
+++ server/app/serializers/post_serializer.rb
@@ -1,12 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :created_at
+  attributes :id, :title, :url, :points, :created_at
   has_one :user do
     [username: object.user.username, user_id: object.user.id]
   end
   has_many :comments do
     object.comments.length
-  end
-  has_one :vote_count do
-    object.vote_total
   end
 end
