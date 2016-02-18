317231330be91a2e3e39e969130457f8f7ed7d21
louisscruz <louisstephancruz@me.com>
2016-02-17 23:12:48 -0500

    Refactor vote tests slightly, and refactor vote model

--- server/app/models/vote.rb
+++ server/app/models/vote.rb
@@ -1,35 +1,18 @@
 class Vote < ApplicationRecord
-  after_destroy :remove_points!
+  after_destroy { update_points!(-1)}
   after_create :update_points!
-  after_update { update_points!(true) }
-  after_update :log_update
+  after_update { update_points!(2) }
   belongs_to :votable, polymorphic: true
   belongs_to :user
   validates_presence_of :votable
   validates_presence_of :user_id
   validates :user_id, uniqueness: { scope: [:votable_type, :votable_id] }
 
-  def update_points!(actual_update=false)
-    resource = self.votable
-    resource.with_lock do
+  def update_points!(i=1)
+    self.with_lock do
+      resource = self.votable
       value = resource.points
-      if actual_update
-        resource.update_attribute :points, value + (self.polarity * 2)
-      else
-        resource.update_attribute :points, value + self.polarity
-      end
+      resource.update_attribute :points, value + (self.polarity * i)
     end
-  end
-
-  def remove_points!
-    resource = self.votable
-    resource.with_lock do
-      value = resource.points
-      resource.update_attribute :points, value - self.polarity
-    end
-  end
-
-  def log_update
-    p "attempted update"
   end
 end
--- server/test/models/vote_test.rb
+++ server/test/models/vote_test.rb
@@ -4,6 +4,7 @@ class VoteTest < ActiveSupport::TestCase
   def setup
     FactoryGirl.create(:user)
     FactoryGirl.create(:post)
+    User.create(id: 2, username: "johndoe2", email: "a@b.com", password: "testtest", password_confirmation: "testtest")
     @vote = Vote.create(votable: Post.first, user_id: User.first.id, polarity: 1)
     @downvote = Vote.create(votable: Post.first, user_id: User.first.id, polarity: -1)
   end
@@ -25,38 +26,30 @@ class VoteTest < ActiveSupport::TestCase
   test "should not allow multiple votes by one user" do
     duplicate_vote = @vote.dup
     duplicate_vote.polarity = -1
-    #@vote.save
     assert_not duplicate_vote.valid?
   end
 
   test "should allow several votes by one user on different objects" do
     duplicate_vote = @vote.dup
     duplicate_vote.votable = Post.second
-    #@vote.save
     assert_not duplicate_vote.valid?
   end
 
   test "should increment tally of points for posts when upvote" do
     old_points = Post.first.points
-    User.create(id: 2, username: "johndoe2", email: "a@b.com", password: "testtest", password_confirmation: "testtest")
     upvote = Vote.create(votable: Post.first, user_id: 2, polarity: 1)
-    #upvote.save
     assert_equal old_points + 1, upvote.votable.points
   end
 
   test "should reduce tally of points for posts when downvote" do
     old_points = Post.first.points
-    User.create(id: 3, username: "johndoe2", email: "a@b.com", password: "testtest", password_confirmation: "testtest")
-    upvote = Vote.create(votable: Post.first, user_id: 3, polarity: -1)
-    #upvote.save
+    upvote = Vote.create(votable: Post.first, user_id: 2, polarity: -1)
     assert_equal old_points - 1, upvote.votable.points
   end
 
   test "should negate tally of points for posts when vote deleted" do
     old_points = Post.first.points
-    User.create(id: 2, username: "johndoe2", email: "a@b.com", password: "testtest", password_confirmation: "testtest")
     upvote = Vote.create(votable: Post.first, user_id: 2, polarity: 1)
-    #upvote.save
     assert_equal old_points + 1, upvote.votable.points
     upvote.destroy
     assert_equal old_points, upvote.votable.points
@@ -64,11 +57,17 @@ class VoteTest < ActiveSupport::TestCase
 
   test "should reverse vote when change from upvote to downvote" do
     old_points = Post.first.points
-    User.create(id: 2, username: "johndoe2", email: "a@b.com", password: "testtest", password_confirmation: "testtest")
     upvote = Vote.create(votable: Post.first, user_id: 2, polarity: 1)
-    #upvote.save
     assert_equal old_points + 1, upvote.votable.points
     upvote.update_attribute(:polarity, -1)
     assert_equal old_points - 1, upvote.votable.points
+  end
+
+  test "should reverse vote when change from downvote to upvote" do
+    old_points = Post.first.points
+    upvote = Vote.create(votable: Post.first, user_id: 2, polarity: -1)
+    assert_equal old_points - 1, upvote.votable.points
+    upvote.update_attribute(:polarity, 1)
+    assert_equal old_points + 1, upvote.votable.points
   end
 end
