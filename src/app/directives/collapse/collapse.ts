4cd0def1a8954514d028d312c4737562b9b81d4b
louisscruz <louisstephancruz@me.com>
2016-03-16 20:02:28 -0400

    Add comment form

---
+++ src/app/directives/collapse/collapse.ts
@@ -0,0 +1,85 @@
+import {Directive, Input, HostBinding} from 'angular2/core';
+
+// todo: add animate
+// todo: add init and on change
+@Directive({selector: '[collapse]'})
+export class Collapse {
+  // style
+  @HostBinding('style.height')
+  private height:string;
+  @HostBinding('style.padding')
+  private padding:string;
+  @HostBinding('style.opacity')
+  private opacity:string;
+  @HostBinding('style.transition')
+  private transition:string;
+  // shown
+  @HostBinding('class.in')
+  @HostBinding('attr.aria-expanded')
+  private isExpanded:boolean = true;
+  // hidden
+  @HostBinding('attr.aria-hidden')
+  private isCollapsed:boolean = false;
+  // stale state
+  @HostBinding('class.collapse')
+  private isCollapse:boolean = true;
+  // animation state
+  @HostBinding('class.collapsing')
+  private isCollapsing:boolean = false;
+
+  @Input()
+  private set collapse(value:boolean) {
+    this.isExpanded = value;
+    this.toggle();
+  }
+
+  private get collapse():boolean {
+    return this.isExpanded;
+  }
+
+  constructor() {
+    this.height = '0';
+    this.padding = '0rem';
+    this.opacity = '0';
+    this.transition = 'all 0.2s ease-in-out';
+  }
+
+  toggle() {
+    if (this.isExpanded) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+
+  hide() {
+    this.isCollapse = false;
+    this.isCollapsing = true;
+
+    this.isExpanded = false;
+    this.isCollapsed = true;
+    setTimeout(() => {
+      this.height = '0';
+      this.padding = '0rem';
+      this.opacity = '0';
+      this.isCollapse = true;
+      this.isCollapsing = false;
+    }, 4);
+  }
+
+  show() {
+    this.isCollapse = false;
+    this.isCollapsing = true;
+
+    this.isExpanded = true;
+    this.isCollapsed = false;
+    setTimeout(() => {
+      this.height = 'auto';
+      this.opacity = '1';
+      this.padding = '0.75rem 1.25rem';
+
+      this.isCollapse = true;
+      this.isCollapsing = false;
+    }, 4);
+  }
+}
--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,14 +1,23 @@
 import {Component, OnInit} from 'angular2/core';
 import {Router, RouteParams, RouterLink} from 'angular2/router';
 import {DROPDOWN_DIRECTIVES} from '../directives/dropdown';
+import {
+  FORM_DIRECTIVES,
+  FormBuilder,
+  ControlGroup,
+  Validators,
+  AbstractControl,
+  Control
+} from 'angular2/common';
 
 import {Post} from '../datatypes/post/post';
 import {AuthService} from '../services/auth/authService';
 import {PostService} from '../services/posts/postsService';
+import {Collapse} from '../directives/collapse/collapse';
 
 @Component({
   selector: 'post-detail',
-  directives: [RouterLink, DROPDOWN_DIRECTIVES],
+  directives: [RouterLink, DROPDOWN_DIRECTIVES, Collapse],
   providers: [PostService],
   styles: [ require('./post.scss') ],
   template: require('./post.html')
@@ -17,13 +26,23 @@ import {PostService} from '../services/posts/postsService';
 export class PostDetail implements OnInit {
   private post: Post;
   private comments: Array<any>;
+  private commentForm: ControlGroup;
+  private comment: AbstractControl;
+  public isCollapsed: boolean = true;
 
   constructor(
     private _router: Router,
     private _routeParams: RouteParams,
     private _authService: AuthService,
-    private _postsService: PostService
-  ) { }
+    private _postsService: PostService,
+    private _fb: FormBuilder
+  ) {
+    this.commentForm = _fb.group({
+      'comment': ['', Validators.compose([
+        Validators.required])]
+    });
+    this.comment = this.commentForm.controls['password'];
+  }
 
   ngOnInit() {
     let id = this._routeParams.get('id');
