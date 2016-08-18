5bff04e32f3491fa359db4689ce2470367435de0
louisscruz <louisstephancruz@me.com>
2016-08-17 21:32:32 -0700

    Update the email validator and update login form

--- src/app/directives/emailValidator/email.validator.ts
+++ src/app/directives/emailValidator/email.validator.ts
@@ -5,10 +5,8 @@ function validateEmailFactory(/*emailBlackList: EmailBlackList*/) {
   return (c: FormControl) => {
     let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-    return EMAIL_REGEXP.test(c.value) ? null : {
-      validateEmail: {
-        valid: false
-      }
+    if (c.value.length > 0 && !EMAIL_REGEXP.test(c.value)) {
+      return { invalidEmail: true }
     }
   }
 }
