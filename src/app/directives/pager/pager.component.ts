a81a5d30cfed4fc5259ca546cdc24d24da597052
louisscruz <louisstephancruz@me.com>
2016-08-22 12:27:47 -0700

    Add pageRemaining to pager

--- src/app/directives/pager/pager.component.ts
+++ src/app/directives/pager/pager.component.ts
@@ -6,10 +6,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class PagerComponent {
   @Input() currentPage: number;
+  @Input() totalItems: number;
+  @Input() perPage: number;
 
   @Output() pageChanged = new EventEmitter();
 
   constructor() {}
+
+  pagesRemaining(): boolean {
+    return (this.totalItems - (this.currentPage * this.perPage) > 0)
+  }
 
   selectPage(page): void {
     this.pageChanged.emit({
