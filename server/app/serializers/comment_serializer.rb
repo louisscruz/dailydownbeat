4d7cd541bc2adf5b315fea0090f4d93550608e39
louisscruz <louisstephancruz@me.com>
2016-08-30 15:07:26 -0700

    Add comment upvoted downvoted to serializer

--- server/app/serializers/comment_serializer.rb
+++ server/app/serializers/comment_serializer.rb
@@ -4,4 +4,22 @@ class CommentSerializer < ActiveModel::Serializer
   def user
     { id: object.user.id, username: object.user.username }
   end
+
+  def upvoted
+    if scope
+      user_upvotes = Vote.where(user_id: this_user.id, polarity: 1)
+      user_upvotes.any? { |user_vote| user_vote.votable_id == self.id }
+    else
+      false
+    end
+  end
+
+  def downvoted
+    if scope
+      user_downvotes = Vote.where(user_id: this_user.id, polarity: -1)
+      user_downvotes.any? { |user_vote| user_vote.votable_id == self.id }
+    else
+      false
+    end
+  end
 end
