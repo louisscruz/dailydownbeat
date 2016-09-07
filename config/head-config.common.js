4e7fe28647330ba301d6ea4b5c0a424f3c781357
louisscruz <louisstephancruz@me.com>
2016-09-07 01:10:48 -0700

    Update ico icon

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -16,15 +16,7 @@
 module.exports = {
   link: [
     /** <link> tags for "apple-touch-icon" (AKA Web Clips). **/
-    { rel: "apple-touch-icon", sizes: "57x57", href: "/assets/icon/apple-icon-57x57.png" },
-    { rel: "apple-touch-icon", sizes: "60x60", href: "/assets/icon/apple-icon-60x60.png" },
-    { rel: "apple-touch-icon", sizes: "72x72", href: "/assets/icon/apple-icon-72x72.png" },
-    { rel: "apple-touch-icon", sizes: "76x76", href: "/assets/icon/apple-icon-76x76.png" },
-    { rel: "apple-touch-icon", sizes: "114x114", href: "/assets/icon/apple-icon-114x114.png" },
-    { rel: "apple-touch-icon", sizes: "120x120", href: "/assets/icon/apple-icon-120x120.png" },
-    { rel: "apple-touch-icon", sizes: "144x144", href: "/assets/icon/apple-icon-144x144.png" },
-    { rel: "apple-touch-icon", sizes: "152x152", href: "/assets/icon/apple-icon-152x152.png" },
-    { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icon/apple-icon-180x180.png" },
+    { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icon/apple-icon.png" },
 
     /** <link> tags for android web app icons **/
     { rel: "icon", type: "image/png", sizes: "192x192", href: "/assets/icon/android-icon-192x192.png" },
@@ -35,11 +27,12 @@ module.exports = {
     { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/icon/favicon-16x16.png" },
 
     /** <link> tags for a Web App Manifest **/
-    { rel: "manifest", href: "/assets/manifest.json" }
+    { rel: "manifest", href: "/assets/manifest.json" },
+
+    /** <link> safari pinned tab **/
+    { rel: "mask-icon", href: "/assets/icon/safari-pinned-tab.svg", color: "#505050" }
   ],
   meta: [
-    { name: "msapplication-TileColor", content: "#00bcd4" },
-    { name: "msapplication-TileImage", content: "/assets/icon/ms-icon-144x144.png", "=content": true },
-    { name: "theme-color", content: "#00bcd4" }
+    { name: "theme-color", content: "#ffffff" }
   ]
 };
